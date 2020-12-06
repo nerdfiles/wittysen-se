b8dd98004e59c85e3b45f3317150f3e131c9aa18
Aaron <aalexander@zoll.com>
2020-12-06 13:49:54 -0600

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp')
+
+// main tasks
+
+gulp.task('default', function (done) {
+  console.log('default')
+  done()
+})
+
+gulp.task('build', function (done) {
+  console.log('build')
+  done()
+})
+
+gulp.task('develop', function (done) {
+  console.log('develop')
+  done()
+})
+
+gulp.task('compile', function (done) {
+  console.log('compile')
+  done()
+})
+
+// utilities
+
---
+++ src/server/interfaces/http/transitions/index.js
@@ -0,0 +1,8 @@
+const itemForms = require('./itemForms.js');
+const pageForms = require('./pageForms.js');
+
+
+module.exports = {
+  itemForms,
+  pageForms
+};
